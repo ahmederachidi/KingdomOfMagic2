431a296032f0ed58780bf41552eb2ec6e6c144d5
iiTzKahrba <40093910+iiTzKahrba@users.noreply.github.com>
2018-06-27 23:44:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,8 +60,6 @@ if (command == "say") {
 let rank = message.guild.member(message.author).roles.find('name', 'ᘩThe KING ♚');
 if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
   message.channel.send(args.join("  "))
-  message.channel.send('Requested By ${message.author.username}')
-
     message.delete();
   }
 
