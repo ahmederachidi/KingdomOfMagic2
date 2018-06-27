cb3f0c9426f4be18ddbb110ee5a1469453868ca9
iiTzKahrba <40093910+iiTzKahrba@users.noreply.github.com>
2018-06-27 22:54:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = 'k!'
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -31,6 +32,17 @@ client.on('message',function(message) {
         message.channel.send('Hey Im **Kingdom Of Magic Bot**  A Nice Bot Developed By:`<@286088294234718209>`')
 
     }
+});
+
+client.on('message', message => {
+   if(message.content.startsWith(prefix + "invites")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has ${inviteCount} invites.`);
+});
+  }
 });
 
 
