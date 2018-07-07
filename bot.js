7b41147ba7151edc3d12b7f642634b551c61f5b1
iiTzKahrba <40093910+iiTzKahrba@users.noreply.github.com>
2018-07-07 08:33:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const prefix = '#'
 
 client.on('ready', () => {
   console.log('╔[════════════════════════════════════]╗');
